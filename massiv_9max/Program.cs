5890be01c25e248645ec3b4a9b8b635fd7e1684f
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 21:26:06 +0300

    argumentsmassiv

---
+++ massiv_9max/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+int[] Massiv = { 12, 44, 23, 33, 99, 20, 64, 11, 19 };
+
+int max = Max (
+    Max (Massiv[0], Massiv[1], Massiv[2]),
+    Max (Massiv[3], Massiv[4], Massiv[5]),
+    Max (Massiv[6], Massiv[7], Massiv[8])
+);
+
+Console.WriteLine(max);
