45f7c8370c39eff9a85a4b33d305d5dd3f9cef84
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 20:22:10 +0300

    what i doing

---
+++ max_9/Program.cs
@@ -0,0 +1,22 @@
+﻿int xa = 11;
+int xb = 12;
+int xc = 66;
+int xd = 2999;
+int xe = 44;
+int xf = 33;
+int xg = 13;
+int xh = 75;
+int xi = 90;
+
+int max = xa;
+
+if (xb > max) max = xb;
+if (xc > max) max = xc;
+if (xd > max) max = xd;
+if (xe > max) max = xe;
+if (xf > max) max = xf;
+if (xg > max) max = xg;
+if (xh > max) max = xh;
+if (xi > max) max = xi;
+
+Console.WriteLine (max);    
