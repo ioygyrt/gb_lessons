219e8899eb77762ab9100bf8e9dd2ed4ac26faff
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 11:04:15 +0300

    User save

--- hello_user/Program.cs
+++ hello_user/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Привет, додя звать-то тебя как?");
+﻿Console.WriteLine("Привет, додя звать-то тебя как?");
 string username = Console.ReadLine();
 Console.Write("Здравствуй, ");
 Console.Write(username);
